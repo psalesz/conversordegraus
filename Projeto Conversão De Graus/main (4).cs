c5bc5ab99416a69cddbb24d0b0ed745f0cd60b4c
Pedro Sales <113061171+psalesz@users.noreply.github.com>
2022-09-16 18:09:15 -0300

    Update main (4).cs

--- Projeto Conversão De Graus/main (4).cs
+++ Projeto Conversão De Graus/main (4).cs
@@ -22,5 +22,25 @@ public class HelloWorld
           if (repetir == 'N') {
           Console.WriteLine("Seu Programa Para Conversão de Graus Celsius(C) para Fahrennheit(F) Está encerrado");
           }
+          while (repetir != 'S') {
+          Console.WriteLine("Digite Ou S ou N por favor...");
+          repetir = char.Parse(Console.ReadLine());
+          }
+          if (repetir == 'S') {
+          while (repetir == 'S') {
+        Console.WriteLine ("Digite uma temperatura em Celsius(C) para ser convertida:");
+        double Tc = double.Parse(Console.ReadLine());
+        
+        double Tf;
+        Tf = 9.0 * Tc / 5.0 + 32.0;
+        
+        Console.WriteLine("Equivalente Em Fahrennheit: " + Tf. ToString("F1", CultureInfo.InvariantCulture));
+        Console.WriteLine("Deseja repetir a Conversão digite  S para Repetir e N para não repetir ");
+          repetir = char.Parse(Console.ReadLine());
+          }
+          if (repetir == 'N') {
+          Console.WriteLine("Seu Programa Para Conversão de Graus Celsius(C) para Fahrennheit(F) Está encerrado");
+          }
+          }
     }
 }
